37e195fdb0638c007d87b9e71f8f0d08d0ea80d1
chan0-kr <iechan06718@gmail.com>
2025-08-25 02:33:06 +0900

    Update background.js

--- background.js
+++ background.js
@@ -8,6 +8,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `${chosenImage}`;
 
 document.body.appendChild(bgImage);
